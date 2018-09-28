2212181025360037493d138f2c5a0568d9ff4fc4
luizanao <luizfelipe.unesp@gmail.com>
2018-09-28 21:09:24 +0100

    Script refactor, code improvements and PEP8

--- apod.py
+++ apod.py
@@ -5,118 +5,93 @@ import json
 import os
 import sys
 import getopt
-import datetime
+from datetime import datetime
 import time
+import argparse
 
-#parse and validate the command line arguments
-def parse_parameters(argv):
+TODAY = datetime.now()
+TODAY_AS_STR = datetime.strftime(TODAY, '%Y-%m-%d')
+FIRST_DATE = datetime.strptime('1995-06-16', '%Y-%m-%d')
 
-    #get the current date and the first date of APOD image
-    today = time.strptime(time.strftime('%Y-%m-%d'), '%Y-%m-%d')
-    first_date = time.strptime('1995-06-16', '%Y-%m-%d')
+API_URL = "https://api.nasa.gov/planetary/apod"
+API_PARAMS = '?api_key={}&date={}'
 
-    path = ''
-    date = today
-    key = 'DEMO_KEY'
+def lookup_image(date, api_key, path):
+    """
+    Lookup and save image from NASA API based on given date.
 
-    #parse tge args
-    try:
-        opts, args = getopt.getopt(argv[1:], 'p:d:k:h', ['path=', 'date=', 'key='])
+    See more on: https://api.nasa.gov/api.html
+    :param str date: Valid date as String
+    :param str api_key: NASA api key
+    :param str path: Path on host to where image will be downloaded
+    :return: None
+    :raises OSError: if there's no permission to store image (create dir or write image)
 
-    except getopt.GetoptError as err:
-        usage()
+    """
+    # Create the API url
+    api_url = API_URL + API_PARAMS.format(api_key, date)
 
-    except:
-        usage()
+    # Get the json with metadata of the image
+    response = urllib.urlopen(api_url)
 
-    for o, a in opts:
-        
-        if o in ('-p', '--path'):
-            path = a
-        
-        elif o in ('-d', '--date'):
+    response = json.loads(response.read())
+    if not (isinstance(response, dict) and 'url' in response):
+        print 'Error parsing response. Responde json might has changed?'
+        sys.exit(2)
 
-            date = a
-
-            try:
-
-                #verify the date format and range
-                datetime.datetime.strptime(date, '%Y-%m-%d')
-                date = time.strptime(date, '%Y-%m-%d')
-
-                if date > today or date < first_date:
-                    print 'Date out of range [1995-06-16 - today]'
-                    sys.exit(2)
-
-            except ValueError:
-                print 'Incorrect data format, should be YYYY-MM-DD'
-                usage()
-
-        elif o in ('-k', '--key'):
-            key = a
-
-        elif o == '-h':
-            usage()
-
-        else:
-            usage()
-
-    if len(opts) == 0: usage()
-
-    return {'path': path, 'date': date, 'key' : key}
-
-
-def usage():
-
-    print """<script>.py [options]
-    -d, --date= the date of the Astronomy Picture (YYYY-MM-DD)
-    -h show this help
-    -k, --key = the key of NASA APOD API
-    -p, --path= the image destination path"""
-
-    sys.exit(2)
-
-
-def get_image(args):
-
-    formated_date = time.strftime('%Y-%m-%d', args['date'])
-    
-    #create the API url
-    api_url = 'https://api.nasa.gov/planetary/apod?api_key=' + args['key'] + '&date=' + formated_date + '&format=JSON'
-
-    #get the json with metadata of the image
-    response = urllib.urlopen(api_url)      
-
-    #get the image URL
-    image_url = json.loads(response.read())['url']
+    image_url = response.get("url")
 
     file_extension = os.path.splitext(image_url)[1]
 
-    path = args['path']
     if path and path[-1] != '/':
         path = path + '/'
 
-    #try to create the dir (if it not exists)
-    if(path and not os.path.isdir(path)):
-
-        try:
-            os.mkdir(path)
-
-        #permission denied
-        except OSError:
-            print 'Permission denied. Please run: sudo <script>.py [options]'
-            sys.exit(2)
-
-    #save the image on the selected destination
+    # Try to create the dir (if it not exists)
     try:
-        urllib.urlretrieve(image_url, path + formated_date + file_extension)
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        urllib.urlretrieve(image_url, path + date + file_extension)
+        # Permission denied
+    except OSError:
+        raise 'Permission denied. Please run: sudo <script>.py [options]'
+        sys.exit(2)
 
-    except IOError:
-        print 'Permission denied. Please run: sudo <script>.py [options]'
-        sys.exit(2) 
+def valid_date(date):
+    """
+    Type Validator function for argparse.
+
+    Check date format and valida date range
+
+    :param str date: String containing a date to lookup image
+    :return: Valid date as String
+    :rtype: str
+    :raises ValueError: if non-valid date format passed or
+                        out of range date
+
+    """
+    try:
+        _date = datetime.strptime(date, '%Y-%m-%d')
+        if _date > TODAY or _date < FIRST_DATE:
+            msg = 'Date out of range [1995-06-16 - today]'
+            raise argparse.ArgumentTypeError(msg)
+        return date
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("lala")
+    parser.add_argument("-d", "--date",
+                        help="Format YYYY-MM-DD (default: today)",
+                        default=TODAY_AS_STR,
+                        type=valid_date)
+    parser.add_argument("-k", "--key",
+                        help="NASA API key - Get yours at https://api.nasa.gov/index.html#apply-for-an-api-key",
+                        required=True)
+    parser.add_argument("-p", "--path",
+                        help="Path to download image",
+                        required=True)
 
-    args = parse_parameters(sys.argv)
-    
-    get_image(args)
+    args = parser.parse_args()
+    lookup_image(args.date, args.key, args.path)
